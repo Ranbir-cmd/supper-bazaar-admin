813098245aa2947ae7e855c2e3e7ce971b32adba
uplRanabir <ranabir.mandal@utplco.com>
2024-01-03 01:24:16 +0530

    admin dashboard project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <div>
+            <p>Admin dashboard</p>
+            <Button className='text-xl'>Click me</Button>
+        </div>
+
+    )
+}
